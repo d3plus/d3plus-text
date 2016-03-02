ec2890990c82695940e3a37fc6a3a8ce4e02a9f6
Dave Landry <landry.dave@gmail.com>
2016-03-01 21:18:43 -0500

    hopeful test fix

--- test/browser/box.js
+++ test/browser/box.js
@@ -1,50 +1,63 @@
 import {test} from "tape";
 import {default as nightmare} from "nightmare";
 
-const page = nightmare().goto("http://localhost:4000/test/browser/box.html");
+const url = "http://localhost:4000/test/browser/box.html";
 
 test("automatically added <svg> element to page", (assert) => {
-  page.evaluate(() => document.getElementsByTagName("svg").length)
-    .then((result) => assert.equal(1, result))
-    .then(() => assert.end());
+  const page = nightmare();
+  page.goto(url)
+    .evaluate(() => document.getElementsByTagName("svg").length)
+    .then((result) => {
+      assert.equal(1, result);
+      page.end(() => {
+        assert.end();
+      });
+    });
 });
 
-// test("created <text> container element", (assert) => {
-//   assert.plan(1);
-//   page
-//     .evaluate(() => document.getElementsByTagName("text").length)
-//     .then((result) => assert.equal(1, result));
-//   assert.end();
-// });
+test("created <text> container element", (assert) => {
+  const page = nightmare();
+  page.goto(url)
+    .evaluate(() => document.getElementsByTagName("text").length)
+    .then((result) => {
+      assert.equal(1, result);
+      page.end(() => {
+        assert.end();
+      });
+    });
+});
 
-// test("created 2 <tspan> elements", (assert) => {
-//   page
-//     .evaluate(() => document.getElementsByTagName("tspan").length)
-//     .then((result) => {
-//       assert.equal(2, result);
-//       assert.end();
-//     });
-//   // assert.end();
-// });
+test("created 2 <tspan> elements", (assert) => {
+  const page = nightmare();
+  page.goto(url)
+    .evaluate(() => document.getElementsByTagName("tspan").length)
+    .then((result) => {
+      assert.equal(2, result);
+      page.end(() => {
+        assert.end();
+      });
+    });
+});
 
-// test("wrapped text correctly", (assert) => {
-//   page
-//     .evaluate(() => {
-//
-//       const tspans = document.getElementsByTagName("tspan");
-//
-//       function getText(t) {
-//         return t.textContent || t.innerText;
-//       }
-//
-//       return getText(tspans[0]) === "Hello D3plus, please wrap" &&
-//              getText(tspans[1]) === "this sentence for me.";
-//
-//     })
-//     .then((result) => assert.equal(true, result));
-//   assert.end();
-// });
+test("wrapped text correctly", (assert) => {
+  const page = nightmare();
+  page.goto(url)
+    .evaluate(() => {
 
-test.onFinish(() => {
-  setTimeout(() => page.end(), 1000);
+      const tspans = document.getElementsByTagName("tspan");
+
+      function getText(t) {
+        return t.textContent || t.innerText;
+      }
+
+      return getText(tspans[0]) === "Hello D3plus, please wrap" &&
+             getText(tspans[1]) === "this sentence for me.";
+
+    })
+    .then((result) => {
+      assert.equal(true, result);
+      page.end(() => {
+        assert.end();
+      });
+    });
 });
