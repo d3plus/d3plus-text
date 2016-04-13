057e6b8c60a7665fcce228d9cd210a98e95d28f1
Dave Landry <landry.dave@gmail.com>
2016-04-12 22:59:38 -0400

    switches test formatting from faucet to tap-spec

--- test/box.js
+++ test/box.js
@@ -4,25 +4,17 @@ import {default as box} from "../src/box.js";
 const data = {"text": "Hello D3plus, please wrap this sentence for me."};
 box().data([data]).fontFamily("Verdana").fontSize(14)();
 
-test("automatically added <svg> element to page", (assert) => {
-  assert.equal(document.getElementsByTagName("svg").length, 1);
+test("creating DOM elements", (assert) => {
+  assert.equal(document.getElementsByTagName("svg").length, 1, "automatically added <svg> element to page");
+  assert.equal(document.getElementsByTagName("text").length, 1, "created <text> container element");
+  assert.equal(document.getElementsByTagName("tspan").length, 2, "created 2 <tspan> elements");
   assert.end();
 });
 
-test("created <text> container element", (assert) => {
-  assert.equal(document.getElementsByTagName("text").length, 1);
-  assert.end();
-});
-
-test("created 2 <tspan> elements", (assert) => {
-  assert.equal(document.getElementsByTagName("tspan").length, 2);
-  assert.end();
-});
-
-test("wrapped text correctly", (assert) => {
+test("wrapping text correctly", (assert) => {
   const tspans = document.getElementsByTagName("tspan");
 
   assert.true(tspans[0].textContent === "Hello D3plus, please wrap" &&
-              tspans[1].textContent === "this sentence for me.");
+              tspans[1].textContent === "this sentence for me.", "2 line test");
   assert.end();
 });
