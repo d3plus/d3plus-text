8256b34e7547a6e750102269fec81ebfe9aeb2d6
Dave Landry <landry.dave@gmail.com>
2016-05-23 12:43:48 -0400

    fixes unicode usage in split tests

--- test/split.js
+++ test/split.js
@@ -4,8 +4,10 @@ import {default as split, splitChars} from "../src/split.js";
 test("split", (assert) => {
 
   for (let i = 0; i < splitChars.length; i++) {
-    const char = splitChars[i];
-    const arr = split(`test${char}test`);
+    let char = splitChars[i];
+    if (char.startsWith("u")) char = String.fromCharCode(`0x${char.slice(1)}`);
+    const sentence = `test${char}test`;
+    const arr = split(sentence);
     const first = char === " " ? "test" : `test${char}`;
     assert.true(arr[0] === first && arr[1] === "test", `split using "${char}"`);
   }
