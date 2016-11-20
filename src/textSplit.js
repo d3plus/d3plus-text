6141cfeda9790fcfd49b602de4ea6548d800182e
Dave Landry <landry.dave@gmail.com>
2016-11-20 02:11:20 -0200

    removes console statement

--- src/textSplit.js
+++ src/textSplit.js
@@ -60,7 +60,6 @@ const splitAllChars = new RegExp(`(\\${prefixChars.join("|\\")})*[${noSpaceRange
 export default function(sentence) {
   if (!noSpaceLanguage.test(sentence)) return stringify(sentence).match(splitWords);
   return merge(stringify(sentence).match(splitWords).map(d => {
-    console.log(d, japaneseChars.test(d), noSpaceLanguage.test(d));
     if (!japaneseChars.test(d) && noSpaceLanguage.test(d)) return d.match(splitAllChars);
     return [d];
   }));
