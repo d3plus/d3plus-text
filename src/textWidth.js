412f658477a10edd4039be905f62451d4528a1f5
Dave Landry <landry.dave@gmail.com>
2017-01-28 13:41:00 -0300

    adds more robust defaults to textWidth

--- src/textWidth.js
+++ src/textWidth.js
@@ -4,20 +4,27 @@
     @param {String|Array} text Can be either a single string or an array of strings to analyze.
     @param {Object} [style] An object of CSS font styles to apply. Accepts any of the valid [CSS font property](http://www.w3schools.com/cssref/pr_font_font.asp) values.
 */
-export default function(text, style = {"font-size": 10, "font-family": "sans-serif"}) {
+export default function(text, style) {
+
+  style = Object.assign({
+    "font-size": 10,
+    "font-family": "sans-serif",
+    "font-style": "normal",
+    "font-weight": 400,
+    "font-variant": "normal"
+  }, style);
 
   const context = document.createElement("canvas").getContext("2d");
 
   const font = [];
-  if ("font-style" in style) font.push(style["font-style"]);
-  if ("font-variant" in style) font.push(style["font-variant"]);
-  if ("font-weight" in style) font.push(style["font-weight"]);
-  if ("font-size" in style) {
-    let s = `${style["font-size"]}px`;
-    if ("line-height" in style) s += `/${style["line-height"]}px`;
-    font.push(s);
-  }
-  if ("font-family" in style) font.push(style["font-family"]);
+  font.push(style["font-style"]);
+  font.push(style["font-variant"]);
+  font.push(style["font-weight"]);
+  font.push(typeof style["font-size"] === "string" ? style["font-size"] : `${style["font-size"]}px`);
+  // let s = `${style["font-size"]}px`;
+  // if ("line-height" in style) s += `/${style["line-height"]}px`;
+  // font.push(s);
+  font.push(style["font-family"]);
 
   context.font = font.join(" ");
 
