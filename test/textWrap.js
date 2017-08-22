194f6fc3450d38e3e4cf570968f5ca564f7df42c
Dave Landry <landry.dave@gmail.com>
2017-08-21 23:33:25 -0300

    fixes textWrap test

--- test/textWrap.js
+++ test/textWrap.js
@@ -5,7 +5,7 @@ export default zora()
   .test("textWrap", assert => {
 
     const sentence = "Hello D3plus, please wrap this sentence for me.",
-          testWrap = textWrap().fontSize(14)(sentence);
+          testWrap = textWrap().fontFamily("Verdana").fontSize(14)(sentence);
 
     assert.ok(testWrap.lines[0] === "Hello D3plus, please wrap" &&
               testWrap.lines[1] === "this sentence for me.", "returning wrapped lines");
@@ -13,7 +13,7 @@ export default zora()
     assert.equal(testWrap.truncated, false, "returning truncated boolean");
 
     const spaceTest = "Two  Space Test",
-          spaceWrap = textWrap().fontSize(14)(spaceTest);
+          spaceWrap = textWrap().fontFamily("Verdana").fontSize(14)(spaceTest);
     assert.equal(spaceWrap.lines[0], spaceTest, "catch for multiple spaces");
 
     assert.equal(textWrap()("A\nB").lines[0], "A", "catch for literal line break (\\n)");
