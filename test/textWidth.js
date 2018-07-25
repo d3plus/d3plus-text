2864d52ce1f9a0ad797ca5a7253aa7955bb17308
Dave Landry <landry.dave@gmail.com>
2018-07-25 10:06:14 -0400

    changes tests to use "DejaVuSans" font-family (#58)

--- test/textWidth.js
+++ test/textWidth.js
@@ -3,10 +3,12 @@ import {default as textWidth} from "../src/textWidth.js";
 
 test("textWidth", assert => {
 
-  const base = textWidth("Test", {"font-family": "Verdana", "font-size": 14}),
-        bigger = textWidth("Test", {"font-family": "Verdana", "font-size": 28}),
-        bolder = textWidth("Test", {"font-family": "Verdana", "font-size": 14, "font-weight": "bold"}),
-        longer = textWidth("TestTest", {"font-family": "Verdana", "font-size": 14});
+  const font = "DejaVuSans";
+
+  const base = textWidth("Test", {"font-family": font, "font-size": 14}),
+        bigger = textWidth("Test", {"font-family": font, "font-size": 28}),
+        bolder = textWidth("Test", {"font-family": font, "font-size": 14, "font-weight": "bold"}),
+        longer = textWidth("TestTest", {"font-family": font, "font-size": 14});
 
   assert.ok(base * 2 === longer, "string length");
   assert.ok(base < bigger, "font-size");
--- test/textWrap.js
+++ test/textWrap.js
@@ -3,8 +3,10 @@ import {default as textWrap} from "../src/textWrap.js";
 
 test("textWrap", assert => {
 
+  const font = "DejaVuSans";
+
   const sentence = "Hello D3plus, please wrap this sentence for me.",
-        testWrap = textWrap().fontFamily("Verdana").fontSize(14)(sentence);
+        testWrap = textWrap().fontFamily(font).fontSize(14)(sentence);
 
   assert.ok(testWrap.lines[0] === "Hello D3plus, please wrap" &&
             testWrap.lines[1] === "this sentence for me.", "returning wrapped lines");
@@ -12,7 +14,7 @@ test("textWrap", assert => {
   assert.equal(testWrap.truncated, false, "returning truncated boolean");
 
   const spaceTest = "Two  Space Test",
-        spaceWrap = textWrap().fontFamily("Verdana").fontSize(14)(spaceTest);
+        spaceWrap = textWrap().fontFamily(font).fontSize(14)(spaceTest);
   assert.equal(spaceWrap.lines[0], spaceTest, "catch for multiple spaces");
 
   assert.equal(textWrap()("A\nB").lines[0], "A", "catch for literal line break (\\n)");
