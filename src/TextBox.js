974570ba5892811fb698d326216d600b0e0e217d
cnavarreteliz <cnavarreteliz@gmail.com>
2018-08-17 17:01:08 -0300

    minor changes

--- src/TextBox.js
+++ src/TextBox.js
@@ -272,7 +272,8 @@ export default class TextBox extends BaseClass {
             .attr("font-weight", d.fW)
             .style("font-weight", d.fW)
             .attr("x", `${d.tA === "middle" ? d.w / 2 : rtl ? d.tA === "start" ? d.w : 0 : d.tA === "end" ? d.w : 0}px`)
-            .attr("y", (t, i) => d.r === 0 || vA === "top" ? `${(i + 1) * d.lH - (d.lH - d.fS)}px` : vA === "middle" ? `${(d.h + d.fS) / 2 + (d.fS - d.lH)}px` : `${d.h - 2 * (d.lH - d.fS)}px`);
+            .attr("y", (t, i) => d.r === 0 || vA === "top" ? `${(i + 1) * d.lH - (d.lH - d.fS)}px` : 
+              vA === "middle" ? `${(d.h + d.fS) / 2 + (d.fS - d.lH)}px` : `${d.h - 2 * (d.lH - d.fS)}px`);
         }
 
 
