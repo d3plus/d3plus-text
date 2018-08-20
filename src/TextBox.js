e5aefd362027d30145aea565251a519d02ae4b69
cnavarreteliz <cnavarreteliz@gmail.com>
2018-08-20 14:56:53 -0300

    fixes vertical multiline alignment using bottom value

--- src/TextBox.js
+++ src/TextBox.js
@@ -273,7 +273,9 @@ export default class TextBox extends BaseClass {
             .style("font-weight", d.fW)
             .attr("x", `${d.tA === "middle" ? d.w / 2 : rtl ? d.tA === "start" ? d.w : 0 : d.tA === "end" ? d.w : 0}px`)
             .attr("y", (t, i) => d.r === 0 || d.vA === "top" ? `${(i + 1) * d.lH - (d.lH - d.fS)}px` : 
-              d.vA === "middle" ? `${(d.h + d.fS) / 2 + (d.fS - d.lH)}px` : `${d.h - 2 * (d.lH - d.fS)}px`);
+              d.vA === "middle" ? `${d.h / 2 + d.fS / 2 - (d.lH - d.fS)}px` : `${d.h - 2 * (d.lH - d.fS) - (d.lines.length - (i + 1)) * d.lH}px`);
+//i * d.lH + (d.h + d.fS) / 2 + (d.fS - d.lH)
+              console.log(d)
 
         }
 
